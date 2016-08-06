29988c351285c82a82463cffb4fd8e329d0e7b0d
maryfand <547284607@qq.com>
2016-08-06 13:37:27 +0800

    rf

--- src/gitSearch.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-export default function getUserInfo(){
-  return axios.get(`https://api.github.com/users/maryfand`)
-    .then((xx) => (
-      { gitInfo: xx.data }
-  ))
-}
